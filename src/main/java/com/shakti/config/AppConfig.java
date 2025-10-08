5aa8ef459e05a333536334d8db0bb654909d6ab5
Nitin Borkar <nitinborkar22@gmail.com>
2025-10-08 21:23:31 +0530

    receipe-sharing-portal

---
+++ src/main/java/com/shakti/config/AppConfig.java
@@ -0,0 +1,47 @@
+package com.shakti.config;
+
+
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
--- src/main/java/com/shakti/controller/ReceipeController.java
+++ src/main/java/com/shakti/controller/ReceipeController.java
@@ -10,41 +10,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/receipe")
 public class ReceipeController {
     @Autowired
     private ReceipeService receipeService;
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/receipe/user/{userId}")
+    @PostMapping("/user/{userId}")
     public Receipe createReceipe(@RequestBody Receipe receipe, @PathVariable Long userId) throws Exception {
         User user = userService.findUserById(userId);
         Receipe createdReceipe = receipeService.createReceipe(receipe, user);
         return createdReceipe;
     }
 
-    @PutMapping("/api/receipe/{id}")
+    @PutMapping("/{id}")
     public Receipe updateReceipe(@RequestBody Receipe receipe, @PathVariable Long id) throws Exception {
         Receipe updatedReceipe = receipeService.updateReceipe(receipe, id);
         return updatedReceipe;
     }
 
-    @PutMapping("/api/receipe/{receipeId}/user/{userId}/like")
+    @PutMapping("/{receipeId}/like/user/{userId}")
     public Receipe likeReceipe(@PathVariable Long receipeId, @PathVariable Long userId) throws Exception {
         User user = userService.findUserById(userId);
         return receipeService.likeReceipe(receipeId, user);
     }
 
-    @GetMapping("/api/receipe/")
+    @GetMapping("/")
     public List<Receipe> getAllReceipe() {
         List<Receipe> allReceipe = receipeService.findAllReceipe();
         return allReceipe;
     }
 
-    @DeleteMapping("/api/receipe/{recipeId}")
-    public String deleteReceipeById(@PathVariable Long reciepeId) throws Exception {
-        receipeService.findReceipeById(reciepeId);
-        receipeService.deleteReceipe(reciepeId);
-        return "Receipe deleted successfully";
+    @DeleteMapping("/{receipeId}")
+    public String deleteReceipeById(@PathVariable Long receipeId) throws Exception {
+        receipeService.findReceipeById(receipeId);
+        receipeService.deleteReceipe(receipeId);
+        return "Receipe deleted successfully" + receipeId;
     }
 }
